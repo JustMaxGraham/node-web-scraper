dff480ec3e690c0ebd26f8e6a2e3d8d84797320f
MaxHuw <m.huw.graham@gmail.com>
2021-10-18 14:22:38 -0400

    Added instructions

--- scraper.js
+++ scraper.js
@@ -2,25 +2,35 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const pretty = require("pretty");
 
-const url = "https://www.allrecipes.com/recipe/236609/honey-garlic-slow-cooker-chicken-thighs/";
+const url3 = "https://www.allrecipes.com/recipe/219988/cajun-crab-cakes-no-breadcrumbs/"
+const url2 = "https://www.allrecipes.com/recipe/236609/honey-garlic-slow-cooker-chicken-thighs/";
+const url1 = "https://www.allrecipes.com/recipe/260540/chef-johns-sourdough-bread/"
 
 async function scrapeData()  {
   try {
-    const { data } = await axios.get(url);
+    const { data } = await axios.get(url2);
 
     const $ = cheerio.load(data);
 
-    const ingredients = $(".ingredients-section__fieldset ul li")
-    //console.log(ingredients)
+    const ingredients = $(".ingredients-section__fieldset ul li");
+    const instructions = $(".instructions-section__fieldset ul li");
+    //console.log("Instruction section: ", instructions)
+
     const recipe = {
-      ingredients: []
+      ingredients: [],
+      instructions: []
     };
 
     ingredients.each((index, element) => {
-      ingredient = $(element).find(".ingredients-item-name").text()
-      console.log("element", $(element).find(".ingredients-item-name").text())
+      ingredient = $(element).find('.ingredients-item-name').text();
+      //console.log("Ingredient", ingredient)
       recipe.ingredients.push(ingredient);
+    });
 
+    instructions.each((index, element) => {
+      instruction = $(element).find('.paragraph').text();
+      //console.log("Instruction", instruction)
+      recipe.instructions.push(instruction);
     });
     console.log("Recipe: ", recipe)
   } catch(error) {
